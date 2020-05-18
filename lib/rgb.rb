b3a2cdeeff361006cd6fec00e08dd4e06adb9654
k-saunagi <k5.daha55@gmail.com>
2020-05-18 16:40:28 +0900

    rgb.rb に 説明文を追記

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,9 +1,11 @@
+#rgbの色を６文字の文字列に変換
 def to_hex(r,g,b)
   [r,g,b].inject('#') do |hex,n|
 	hex + n.to_s(16).rjust(2,'0')
 end
 end
 
+#６文字の文字列をrgbの色に変換
 def to_ints(hex)
 	r = hex[1..2]
 	g = hex[3..4]
